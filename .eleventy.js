1ede735537b0b4bdc36c655a61d99351759e770b
mollycarroll <mollyajcarroll@gmail.com>
2022-05-21 19:40:47 -0600

    add and test css, adjust index and base layout

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,6 @@
 module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy('images');
     eleventyConfig.addPassthroughCopy('admin');
+    eleventyConfig.addPassthroughCopy('css');
+    eleventyConfig.addWatchTarget('css/');
 };
